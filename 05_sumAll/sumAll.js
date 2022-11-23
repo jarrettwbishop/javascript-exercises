e8344d4c7e223bba933b98932479979d0ceee196
Jarrett <jarrettwbishop@gmail.com>
2022-11-23 14:23:34 -0500

    Create sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    function compareNumbers(a, b) {
+        return a - b;
+    }
+    let arr = Array.from(arguments).sort(compareNumbers);
+    let min = arr[0];
+    let max = arr[1];
 
+    if (min < 0 || max < 0) {
+        return "ERROR"
+    }
+
+    if (isNaN(min) || isNaN(max)) {
+        return "ERROR"
+    }
+
+    let result = 0
+    for (let i = min; i <= max; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
