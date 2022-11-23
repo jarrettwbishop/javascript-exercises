b4f231ccbde726b9436c4eacb82dc11cf4b2f4b9
Jarrett <jarrettwbishop@gmail.com>
2022-11-23 12:58:24 -0500

    Create reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split("")
+    let reversedArr = arr.reverse()
+    return reversedArr.join("")
 };
 
 // Do not edit below this line
