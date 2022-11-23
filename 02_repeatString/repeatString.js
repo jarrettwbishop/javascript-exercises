642a93108d7b3f83b4332931262d350e20ffdc04
Jarrett <jarrettwbishop@gmail.com>
2022-11-23 10:39:50 -0500

    Repeat a string a given number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    result = "";
+    for (let i = 0; i < num; i++) {
+        result += string;
+    }
+    return result;
 
 };
 
