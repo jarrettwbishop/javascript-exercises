fc99bef0b248828eae4be09fcf6b0c6342a8213b
Jarrett Bishop <jarrettwbishop@gmail.com>
2023-05-31 21:58:24 -0400

    Solve palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const isCharacterALetter = function(char) {
+    return char.toLowerCase() != char.toUpperCase()
+}
 
+const palindromes = function (str) {
+    let newStr = ""
+    for (i = 0; i < str.length; i ++) {
+        if (isCharacterALetter(str[i])) {
+            newStr += str[i].toLowerCase();
+        }
+    }
+    strReversed = newStr.split("").reverse().join("");
+    return newStr == strReversed;
+    
 };
 
 // Do not edit below this line
