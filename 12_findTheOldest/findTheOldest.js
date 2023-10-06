c316d408c0df7b66444aaa414b6a3365a020521b
Jarrett Bishop <jarrettwbishop@gmail.com>
2023-10-06 15:57:39 -0400

    Complete assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const calculateAge = function(birth, death) {
+    const date = new Date()
 
+    if (death) {
+        return death - birth;
+    }
+
+    return date.getFullYear() - birth;
+}
+
+const findTheOldest = function(people) {
+    let oldest = people[0];
+
+    people.forEach((person) => {
+        if (calculateAge(person.yearOfBirth, person.yearOfDeath) > calculateAge(oldest.yearOfBirth, oldest.yearOfDeath)) {
+            oldest = person;
+        }
+    })
+
+    return oldest;
 };
 
 // Do not edit below this line
